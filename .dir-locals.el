5a7b470711f9bf3a3c00dcb4ec8a4b4c5f45bfb6
Charanjit Singh <bitspook@protonmail.com>
2022-01-15 13:57:35 +0530

    chore: upgrade flake | make publish-on-save-hook local

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,11 +2,11 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode . ((org-publish-project-alist . (("cryptopals"
-					      :base-directory "./"
-					      :base-exteinsion "org"
-					      :publishing-directory "./web"
-					      :publishing-function org-html-publish-to-html
-					      :auto-preamble t)))
+					                                   :base-directory "./"
+					                                   :base-exteinsion "org"
+					                                   :publishing-directory "./web"
+					                                   :publishing-function org-html-publish-to-html
+					                                   :auto-preamble t)))
               (eval (lambda ()
-                      (add-hook 'after-save-hook #'org-publish-current-project)
+                      (add-hook 'after-save-hook #'org-publish-current-project nil t)
                       (load-file "./orgservable.el"))))))
