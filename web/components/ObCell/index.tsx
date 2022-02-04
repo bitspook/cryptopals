1b8c5c8cbb25dacf2bd6b293ba0f65fef8c45d36
Charanjit Singh <bitspook@protonmail.com>
2022-02-04 17:44:33 +0530

    refactor: use text-area for code blocks in HTML
    It don't have syntax highlighting and all, but it is lightweight af. I tried
    adding codemirror, but it is a whooping 500KB. I need to find a way to remove
    acorn.js; which is around 110KB, while rest of my app is much lighter. May be
    some other day.
    
    I will perhaps add codemirror as an add-on via web-workers or something later.
    It is a nice to have, but I don't want it to slow down the main app.

--- web/components/ObCell/index.tsx
+++ web/components/ObCell/index.tsx
@@ -1,125 +1,119 @@
-import { useCallback, useEffect, useRef, useState } from "preact/hooks";
-import { Inspector } from "@observablehq/runtime";
+import { useCallback, useEffect, useRef, useState } from 'preact/hooks';
+import { Inspector } from '@observablehq/runtime';
 
-import { Interpreter } from "~/web/obhq-compiler/Interpreter";
-import { classnames } from "~/web/utils";
-import * as s from "./style.module.scss";
+import { Interpreter } from '~/web/obhq-compiler/Interpreter';
+import { classnames } from '~/web/utils';
+import * as s from './style.module.scss';
 
 const c = classnames(s);
 
 interface ObCellProps {
   id: string;
-  module: any;                  // Name of the Observable module stored in `moduleRepo`
+  module: any; // Name of the Observable module stored in `moduleRepo`
 }
 
-const ObCell =
-  (modulesRepo: {[name: string]: any}) =>
-  (p: ObCellProps) => {
-    let observer, interpreter;
+const ObCell = (modulesRepo: { [name: string]: any }) => (p: ObCellProps) => {
+  let observer, interpreter;
 
-    // We need to do these shenanigans instead of obtaining a module like a
-    // normal component because web-components can pass only strings as
-    // attributes.
-    const module = modulesRepo[p.module];
+  // We need to do these shenanigans instead of obtaining a module like a
+  // normal component because web-components can pass only strings as
+  // attributes.
+  const module = modulesRepo[p.module];
 
-    if (!module) {
-      console.error('Invalid or missing Observable module for ObCell');
-      return null;
-    }
+  if (!module) {
+    console.error('Invalid or missing Observable module for ObCell');
+    return null;
+  }
 
-    const codeEl = useRef(null);
-    const outputEl = useRef(null);
+  const codeEl = useRef(null);
+  const outputEl = useRef(null);
 
-    const [hasAttention, setHasAttention] = useState(false);
-    const [isEditing, setIsEditing] = useState(false);
-    const [code, setCode] = useState("");
-    // Let's keep the HTML provided by org-mode, and use that for editing.
-    // This saves us from adding a dependency on codemirror or something for now.
-    const [codeHtml, setCodeHtml] = useState("");
+  const [hasAttention, setHasAttention] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [code, setCode] = useState('');
 
-    const cellNode = document.querySelector(`#${p.id}`);
-    const codeNode = cellNode.firstChild;
-    if (!code) setCode(codeNode.textContent);
-    if (!codeHtml) setCodeHtml((codeNode as any).innerHTML);
+  const cellNode = document.querySelector(`#${p.id}`);
+  const codeNode = cellNode.firstChild;
+  if (!code) setCode(codeNode.textContent.trim());
 
-    const handleMouseEnter = useCallback(() => {
-      setHasAttention(true);
-    }, []);
+  const handleMouseEnter = useCallback(() => {
+    setHasAttention(true);
+  }, []);
 
-    const handleMouseLeave = useCallback(() => {
-      setHasAttention(false);
-    }, []);
+  const handleMouseLeave = useCallback(() => {
+    setHasAttention(false);
+  }, []);
 
-    const handleFocus = useCallback(() => {
-      setIsEditing(true);
-    }, []);
+  const handleFocus = useCallback(() => {
+    setIsEditing(true);
+  }, []);
 
-    const handleBlur = useCallback(() => {
-      setIsEditing(false);
-    }, []);
+  const handleBlur = useCallback(() => {
+    setIsEditing(false);
+  }, []);
 
-    const handleCodeChange = useCallback(() => {
-      if (!codeEl.current) return;
+  const runCode = useCallback(() => {
+    if (!codeEl.current) return;
 
-      const newCode = codeEl.current.textContent;
+    const newCode = codeEl.current.value;
 
-      if (newCode === code) return;
-      setCode(newCode);
-    }, [codeEl, code]);
+    if (newCode === code) return;
 
-    useEffect(() => {
-      if (!outputEl.current) return;
+    setCode(newCode);
+  }, [codeEl, code]);
 
-      if (!observer) observer = Inspector.into(outputEl.current);
-      if (!interpreter) interpreter = new Interpreter({ module, observer });
+  useEffect(() => {
+    if (!outputEl.current) return;
 
-      (window as any).interpreter = interpreter;
+    if (!observer) observer = Inspector.into(outputEl.current);
+    if (!interpreter) interpreter = new Interpreter({ module, observer });
 
-      if (!code) return;
+    (window as any).interpreter = interpreter;
 
-      interpreter.module(code);
-    }, [outputEl, code]);
+    if (!code) return;
 
-    // Move the code element added by org-mode into our component for
-    // easier manipulation
-    useEffect(() => {
-      if (!codeEl.current) return;
+    interpreter.module(code).catch((err) => {
+      console.error('Error when evaluating code', err);
+    });
+  }, [outputEl, code]);
 
-      codeEl.current.innerHTML = codeHtml;
-    }, [codeEl, codeHtml]);
+  // Move the code element added by org-mode into our component for
+  // easier manipulation
+  useEffect(() => {
+    if (!codeEl.current || !code) return;
+  }, [codeEl, code]);
 
-    if (!cellNode) {
-      console.error(
-        "Unable to find cell-node for OBCell. [selector=",
-        p.id,
-        "]"
-      );
-      return null;
-    }
+  if (!cellNode) {
+    console.error('Unable to find cell-node for OBCell. [selector=', p.id, ']');
+    return null;
+  }
 
-    return (
-      <div
-        className={c({ container: true, hasAttention, isEditing })}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        onFocus={handleFocus}
-      >
-        <div className={s.gutter}></div>
-        <div className={s.menu}></div>
-        <div className={s.content}>
-          <div className={s.outputContainer} ref={outputEl}></div>
-          <div
-            className={s.codeContainer}
-            ref={codeEl}
-            contentEditable
-            spellcheck={false}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            onKeyUp={handleCodeChange}
-          ></div>
+  return (
+    <div
+      className={c({ container: true, hasAttention, isEditing })}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onFocus={handleFocus}
+    >
+      <div className={s.gutter}></div>
+      <div className={s.content}>
+        <div className={s.outputContainer} ref={outputEl}></div>
+
+        <div className={s.menu}>
+          <button onClick={runCode}>Run</button>
         </div>
+
+        <textarea
+          className={s.codeContainer}
+          ref={codeEl}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        >
+          {code}
+        </textarea>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default ObCell;
